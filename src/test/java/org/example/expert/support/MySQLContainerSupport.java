879fea666517a93b88da5c7fab15170f0b164a7d
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-09-03 17:35:05 +0900

    fix(hotfix): 테스트 환경 및 TodoServiceIntegrationTest 수정
    - @BeforeEach로 테스트 데이터 중복 저장하고 있었음
    - @BeforeAll로 변경해 한 번만 저장하도록 변경
    - Test container 미적용되고 있었음
    - MySQLContainerSupport 내부에 @Bean 등록 안해서 적용 안됨
    - @Bean으로 적용했으나 docker 연결 실패
    - test container bom의 버전과 현재 docker 버전 간의 호환성 문제
    - test container 버전업

--- src/test/java/org/example/expert/support/MySQLContainerSupport.java
+++ src/test/java/org/example/expert/support/MySQLContainerSupport.java
@@ -2,10 +2,12 @@ package org.example.expert.support;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class MySQLContainerSupport {
+    @Bean
     @ServiceConnection
     public MySQLContainer<?> mysqlContainer() {
         return new MySQLContainer<>("mysql:8.0")
